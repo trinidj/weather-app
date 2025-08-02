7a30415478aa8e44fe4d68ede4670e57312b7a5e
trinidj <jm.trinidad199@gmail.com>
2025-08-02 15:08:24 -0400

    feat: add HourlyForecastCard component

---
+++ src/components/ui/HourlyForecastCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <div class="weather-card">
+    <div class="time">
+      <p>12:00</p>
+    </div>
+
+    <div class="weather-image">
+      <img src="../../assets/images/cloud-icon.jpg" />
+    </div>
+
+    <div class="temp">
+      <p>22°</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
