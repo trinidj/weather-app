2c30c18f7d485135e66b5700ef6abcf2049b7ee8
trinidj <jm.trinidad199@gmail.com>
2025-08-04 17:24:44 -0400

    refactor: use object desctructuizing to provide parameters

--- src/components/animations/slideIn.js
+++ src/components/animations/slideIn.js
@@ -1,11 +1,21 @@
+/**
+ * @param { String } targets
+ * @param { Array } translate
+ * @param { Array } opacity
+ * @param { Number } duration
+ * @param { String } easing
+ */
+
 import anime from 'https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.es.js';
 
-export const slideIn = (targets) => {
+export const slideIn = (settings) => {
+  const { targets, translate, opacity, duration, easing } = settings;
+
   anime({
     targets: targets,
-    translateY: [80, 0],
-    opacity: [0, 1],
-    duration: 700,
-    easing: 'easeOutQuad'
+    translateY: translate,
+    opacity: opacity,
+    duration: duration,
+    easing: easing,
   });
 };
