649ebc8c2c07dfa1ba8f32d8d38223b434133e71
trinidj <jm.trinidad199@gmail.com>
2025-08-05 00:14:00 -0400

    feat: add slide-in animation to main weather sections

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,24 @@
   import Layout from '@/components/layout/Layout.vue';
   import WeatherCard from '@/components/WeatherCard.vue';
   import ForecastCard from '@/components/ForecastCard.vue';
+  import { onMounted } from 'vue';
+  import { slideIn } from '@/components/animations/slideIn';
+  
+  onMounted(() => {
+    slideIn({
+      targets: ['#weather-section', '#forecast-section'],
+      translate: [80, 0],
+      opacity: [0, 0.2, 0.5, 0.7, 1],
+      duration: 700,
+      ease: 'inOut',
+    });
+  });
 </script>
 
 <template>
   <Layout>
-    <WeatherCard />
-    <ForecastCard />
+    <WeatherCard id="weather-section"/>
+    <ForecastCard id="forecast-section"/>
   </Layout>
 </template>
 
